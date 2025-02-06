b7039891845fbabd80c82cbfd878c4d78f47963d
azrahive <mdazizurrahim27@gmail.com>
2025-02-07 00:21:15 +0600

    push_pop done

---
+++ array.js
@@ -0,0 +1,30 @@
+// array
+const arrayNumbers=[10,20,30,40,50,60,70];
+console.log(arrayNumbers);
+
+// get array length
+console.log(arrayNumbers.length);
+
+// get element values by index
+console.log(arrayNumbers[5]);
+console.log(arrayNumbers[3]);
+
+// set or update element values by index
+arrayNumbers[3]= 444;
+console.log(arrayNumbers);
+
+arrayNumbers[0]=100;
+console.log(arrayNumbers);
+
+// empty array
+const emptyArrayNumbers=[];
+console.log(emptyArrayNumbers);
+
+
+// ADD element in array using PUSH
+arrayNumbers.push(456);
+arrayNumbers.push(123,345,567);
+console.log(arrayNumbers);
+
+// DELETE element in array using POP
+
---
+++ ternary.js
@@ -0,0 +1,6 @@
+// shorted TERNARY OPERATORS
+// condition ? do something when true: do something when false
+
+const age=66
+
+age>=18 ? console.log("YOU CAN VOTE!") : console.log("YOU CAN NOT VOTE!");
