9d3413a95c3eac6691655b4d7777b333da952080
azrahive <mdazizurrahim27@gmail.com>
2025-02-08 00:03:11 +0600

    practice_sum

---
+++ sum.js
@@ -0,0 +1,28 @@
+// problem:1
+
+const num1 = 100;
+const num2 = 200;
+const sum = num1+num2; // output: 300
+console.log(sum);
+
+// problem:2 
+
+const numm_1 ="100";
+const numm_2 = 200;
+const summ_1 = numm_1+numm_2;
+console.log(summ_1);
+
+// problem:3
+
+const num_1 ="100";
+const num_2 = 200;
+//const convertedNumber= parseInt(num_1) // convert to number
+//const sum_1 = convertedNumber+num_2;
+const sum_1 = (parseInt(num_1)+num_2); 
+console.log(sum_1);
+
+//problem:4
+console.log(10+10+"10") // answer: 2010
+console.log(10+10+parseInt("10")) // answer: 30
+console.log(10+10-"10") // answer: 10
+
